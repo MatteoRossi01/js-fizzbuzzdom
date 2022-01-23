08efc1c4103c0d254794cb47372730f13af5d84b
Matteo Rossi <matteorossii6920@gmail.com>
2022-01-23 16:52:52 +0100

    Esercizio completato

---
+++ main.js
@@ -0,0 +1,18 @@
+var lista = document.getElementById('lista');
+
+for (var i=1; i<101; i++) {
+
+    if (i%15==0) {
+        let i = "fizzBuzz";
+        lista.innerHTML += "<li id=li-lista-5_3>"+i+"</li>";
+    } else if (i%5==0) {
+        let i = "Buzz";
+        lista.innerHTML += "<li id=li-lista-5>"+i+"</li>";
+    } else if (i%3==0){
+        let i = "fizz";
+        lista.innerHTML += "<li id=li-lista-3>"+i+"</li>";
+    }else {
+        lista.innerHTML += "<li id=li-lista>"+i+"</li>";
+    }
+    
+}
